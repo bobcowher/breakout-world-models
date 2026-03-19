65a9cfaf2558e193f19eb944979bfc364cbec279
Robert Cowher <bobcowher@gmail.com>
2026-03-18 19:48:41 -0500

    updated buffer size to 20000

--- train.py
+++ train.py
@@ -6,6 +6,6 @@ gym.register_envs(ale_py)
 
 env = gym.make("ALE/Breakout-v5", render_mode="rgb_array")
 
-agent = Agent(env=env, world_model_batch_size=32, max_buffer_size=10000) 
+agent = Agent(env=env, world_model_batch_size=32, max_buffer_size=20000) 
 
 agent.train(episodes=5000, world_model_epochs=100)
